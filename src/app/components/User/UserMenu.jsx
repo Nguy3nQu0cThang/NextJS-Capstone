7c436a150e3ab304fa9f0804ff36308b231214b2
TuTrieuDuong <tutrieuduong98@gmail.com>
2025-06-01 02:30:51 +0700

    fix lại lỗi bình luận, build giao diện AdminRoom

--- src/app/components/User/UserMenu.jsx
+++ src/app/components/User/UserMenu.jsx
@@ -48,7 +48,7 @@ const UserMenu = ({ isLoggedIn, userName, showModal }) => {
     <div className="user-menu">
       <span className="booking-text">Booking</span>
       <GlobalOutlined className="global-icon" />
-      {isLoggedIn && userName && <div className="user-name">{userName}</div>}
+      {isLoggedIn && userName && <div className="user-name">{userProfile.name}</div>}
       <Dropdown
         menu={{ items: menuItems, onClick: handleMenuClick }}
         trigger={["click"]}
